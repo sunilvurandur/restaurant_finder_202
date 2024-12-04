fc0105635e6bcdea55608b26809283a658f13886
Rohit <jay.dosapati@gmail.com>
2024-12-04 13:16:07 -0800

    Updated App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/HomePage";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ProtectedRoute from "./components/Routes/ProtectedRoute";
 import PublicRoute from "./components/Routes/PublicRoute";
-
+import BusinessOwnerDashboard from "./pages/dashboard/BusinessOwnerDashboard";
+import AdminDashboardApp from "./pages/dashboard/AdminDashboardApp";
+import AdminLogin from "./pages/auth/AdminLogin";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +19,13 @@ const router = createBrowserRouter([
         <HomePage />
       </ProtectedRoute>
     ),
+  },  
+  {
+    path: "/bussiness-owner-dashboard",
+    element :(<PublicRoute><BusinessOwnerDashboard/></PublicRoute>),
+  },{
+    path: "/admin-dashboard",
+    element :(<PublicRoute><AdminDashboardApp/></PublicRoute>),
   },
   {
     path: "/login",
