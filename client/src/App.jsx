e7762bd05e2cfd6e9121467a1feadc6efa070c8a
Rohit <jay.dosapati@gmail.com>
2024-12-04 09:33:07 -0800

    Developed Auth Pages and Auth Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ProtectedRoute from "./components/Routes/ProtectedRoute";
+import PublicRoute from "./components/Routes/PublicRoute";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: (
+      <PublicRoute>
+        <Login />
+      </PublicRoute>
+    ),
+  },
+  {
+    path: "/register",
+    element: (
+      <PublicRoute>
+        <Register />
+      </PublicRoute>
+    ),
+  },
+  {
+    path: "/admin/login",
+    element: (
+      <PublicRoute>
+        <AdminLogin />
+      </PublicRoute>
+    ),
+  },
+
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ToastContainer />
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/shared/Form/InputType.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+
+const InputType = ({
+    labelText,
+    labelFor,
+    inputType,
+    value,
+    onChange,
+    name,
+  }) => {
+    return (
+      <>
+        <div className="mb-1">
+          <label htmlFor={labelFor} className="form-label">
+            {labelText}
+          </label>
+          <input
+            type={inputType}
+            className="form-control"
+            name={name}
+            value={value}
+            onChange={onChange}
+          />
+        </div>
+      </>
+    );
+  };
+  
+  export default InputType;
