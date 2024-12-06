ec438bc2854bdd91e623d3b6406835588109256a
Mahesh-17 <cheekurimahesh@gmail.com>
2024-12-05 22:48:40 -0800

    Routing for get restaurants for business owners

--- backend/src/routes/business_owner_routes.js
+++ backend/src/routes/business_owner_routes.js
@@ -13,5 +13,10 @@ router.post('/createRestaurant',upload.single('photo'), (req, res) =>{
     businessHandler.createRestaurant(req, res);
 })
 
+businessroutes
+router.get('/getRestaurants/:business_owner_id', (req, res) =>{
+    businessHandler.getRestaurants(req, res);
+})
+
 
 module.exports = router;
